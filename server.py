4310333cf350836c2159059df8dc490a0d6f782c
ohgyulim <ohrbfla1004@khu.ac.kr>
2021-11-29 17:09:00 +0900

    connect each other

--- server.py
+++ server.py
@@ -5,64 +5,53 @@ import socket
 import threading
 
 server_ip ='127.0.0.1'
-port = 8080
+port = 8111
 
 pygame.init()
-pygame.display.set_caption('1v1_game')
-height=960
-width=1200
+pygame.display.set_caption('1v1_game_server')
+height=400
+width=800
 
-# enemy_img = pygame.image.load('dd.png')
-# imgw=enemy_img.get_size()[0]
-# imgh=enemy_img.get_size()[1]
-# enex=0
-# eney=imgw
+enemy_img = pygame.image.load('dd.png')
 
 
-enemy_imgDino1 = pygame.image.load('dino1.png')
-enemy_imgDino2 = pygame.image.load('dino2.png')
-enemy_dino_height = enemy_imgDino1.get_size()[1]
-enemy_dino_bottom = height-enemy_dino_height
-enemy_dino_x = 50
-enemy_dino_y = enemy_dino_bottom
-enemy_jump_top =200
-enemy_leg_swap = True
-enemy_is_bottom = True
-enemy_is_go_up = False
+en_dino_height = enemy_img.get_size()[1]
+en_dino_bottom = height - en_dino_height # dino_bottom = 400 - dino 키
+en_dino_x = 50
+en_dino_y = en_dino_bottom
+en_jump_top = 200
+# leg_swap = True
+en_is_bottom = True
+en_is_go_up = False
 
-imgTree = pygame.image.load('tree.png')
-tree_height = imgTree.get_size()[1]
-tree_x = height
-tree_y = height - tree_height
+
+enemy_imgTree = pygame.image.load('tree.png')
+en_tree_height = enemy_imgTree.get_size()[1]
+en_tree_x = width
+en_tree_y = height - en_tree_height
+
 
 
 def consoles():
-    global enemy_dino_x,enemy_dino_y
+    global en_dino_x, en_dino_y,en_is_bottom, en_is_go_up
+
     while True:
-        msg=client.recv(1024)
-        if(msg.decode()=='up' and enemy_is_go_up):
-            enemy_dino_y -= 10.0
-        elif(not enemy_is_go_up and not enemy_is_bottom):
-            enemy_dino_y+=10.0
-        
-        if (enemy_is_go_up and enemy_dino_y <= enemy_jump_top):
-            enemy_is_go_up = False
-        if (not enemy_is_bottom and enemy_dino_y >= enemy_dino_bottom):
-            enemy_is_bottom =True
-            enemy_dino_y = enemy_dino_bottom
-
-
-    # global eney,enex
-    # while True:
-    #     msg=client.recv(1024)
-    #     if(msg.decode()=='up'):
-    #         eney-=30
-    #     elif(msg.decoe()=='down'):
-    #         eney+=30
-    #     elif(msg.decode()=='right'):
-    #         enex+=30
-    #     elif(msg.decode()=='left'):
-    #         enex-=30
+        msg = client.recv(1024)
+        if(msg.decode()=='up'):
+            en_is_go_up = True
+            en_is_bottom = False
+        if en_is_go_up:
+            en_dino_y -=10
+        elif not en_is_go_up and not en_is_bottom:
+            en_dino_y += 10.0
+ 
+        # dino top and bottom check
+        if en_is_go_up and en_dino_y <= en_jump_top:
+            en_is_go_up = False
+ 
+        if not en_is_bottom and en_dino_y >= en_dino_bottom:
+            en_is_bottom = True
+            en_dino_y = en_dino_bottom
 
 def acceptC():
     global client, server, addr
@@ -76,32 +65,28 @@ def acceptC():
     thr.start()
 
 def GameMain():
-    # set screen, fps
-    screen = pygame.display.set_mode((width, height))
-    fps = pygame.time.Clock()
- 
-    # dino
-    imgDino1 = pygame.image.load('dino1.png')
-    imgDino2 = pygame.image.load('dino2.png')
-    dino_height = imgDino1.get_size()[1]
-    dino_bottom = height - dino_height
-    dino_x = 50
-    dino_y = dino_bottom
-    jump_top = 200
-    leg_swap = True
-    is_bottom = True
-    is_go_up = False
- 
-    # tree
+    global en_dino_x,en_dino_y,en_is_bottom, en_is_go_up, en_tree_x,en_tree_y,en_jump_top,enemy_imgTree, enemy_img
+    img = pygame.image.load('dd.png')
+    imgh=img.get_size()[1]
+    imgw=img.get_size()[0]
+    x=50
+    dino_bottom = height - imgh
+    y=dino_bottom
+    is_bottom=True
+    is_go_up =False
+    jump_top=200
+
     imgTree = pygame.image.load('tree.png')
     tree_height = imgTree.get_size()[1]
     tree_x = width
-    tree_y = height - tree_height
- 
+    tree_y = height - en_tree_height
+
+    screen= pygame.display.set_mode((width,height))
+    fps = pygame.time.Clock()
+
     while True:
-        screen.fill((255, 255, 255))
- 
-        # event check
+        screen.fill((255,255,255))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -110,106 +95,53 @@ def GameMain():
                 if is_bottom:
                     is_go_up = True
                     is_bottom = False
- 
-        # dino move
+                    msg='up'
+                    client.sendall(msg.encode())
         if is_go_up:
-            dino_y -= 10.0
+            y -= 10.0
         elif not is_go_up and not is_bottom:
-            dino_y += 10.0
- 
-        # dino top and bottom check
-        if is_go_up and dino_y <= jump_top:
+            y += 10.0
+        
+        if is_go_up and y <= jump_top:
             is_go_up = False
  
-        if not is_bottom and dino_y >= dino_bottom:
+        if not is_bottom and y >= dino_bottom:
             is_bottom = True
-            dino_y = dino_bottom
- 
-        # tree move
+            y = dino_bottom
+        
         tree_x -= 12.0
         if tree_x <= 0:
             tree_x = width
+
+
+        if en_is_go_up:
+            en_dino_y -= 10.0
+        elif not en_is_go_up and not en_is_bottom:
+            en_dino_y += 10.0
+        
+        if en_is_go_up and en_dino_y <= en_jump_top:
+            en_is_go_up = False
  
-        # draw tree
+        if not en_is_bottom and en_dino_y >= en_dino_bottom:
+            en_is_bottom = True
+            en_dino_y = en_dino_bottom
+
+        en_tree_x -= 12.0
+        if en_tree_x <= 0:
+            en_tree_x = width
+
+
+        screen.blit(img,(x,y))
+        screen.blit(enemy_img,(en_dino_x,en_dino_y))
         screen.blit(imgTree, (tree_x, tree_y))
- 
-        # draw dino
-        if leg_swap:
-            screen.blit(imgDino1, (dino_x, dino_y))
-            leg_swap = False
-        else:
-            screen.blit(imgDino2, (dino_x, dino_y))
-            leg_swap = True
- 
-        # update
+        screen.blit(enemy_imgTree, (en_tree_x, en_tree_y))
+
+
         pygame.display.update()
         fps.tick(30)
-
-
-    # global eney, enex
-    # screen = pygame.display.set_mode((width,height))
-    # fps = pygame.time.Clock()
-
-    # img = pygame.image.load('dd.png')
-    # imgh=img.get_size()[1]
-    # imgw=img.get_size()[0]
-    # x=0
-    # y=imgw
-
-    # while True:
-    #     screen.fill((255,255,255))
-
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             pygame.quit()
-    #             sys.exit()
-    #         elif event.type == pygame.KEYDOWN:
-    #             if event.key == pygame.K_UP:
-    #                 pyautogui.keyUp('up')
-    #                 y-=30
-    #                 msg="up"
-    #                 client.sendall(msg.encode())
-    #             elif event.key == pygame.K_DOWN:
-    #                 pyautogui.keyUp('down')
-    #                 y+=30
-    #                 msg="down"
-    #                 client.sendall(msg.encode())
-    #             elif event.key == pygame.K_RIGHT:
-    #                 pyautogui.keyUp('right')
-    #                 x+=30
-    #                 msg="right"
-    #                 client.sendall(msg.encode())
-    #             elif event.key == pygame.K_LEFT:
-    #                 pyautogui.keyUp('left')
-    #                 x-=30
-    #                 msg="left"
-    #                 client.sendall(msg.encode())
-        
-    #     if img.get_size()[0]+x >= width:
-    #         x=width-img.get_size()[0]
-    #     elif x <= 0:
-    #         x=0
-    #     if img.get_size()[1]+y >= height:
-    #         y=height-img.get_size()[1]
-    #     elif y <=0:
-    #         y=0
-        
-    #     if enemy_img.get_size()[0]+enex >=width:
-    #         enex=width-enemy_img.get_size()[0]
-    #     elif enex <=0:
-    #         enex=0
-    #     if enemy_img.get_size()[1]+eney >= height:
-    #         eney=height-enemy_img.get_size()[1]
-    #     elif eney <=0:
-    #         eney =0
-        
-    #     screen.blit(img,(x,y))
-    #     screen.blit(enemy_img,(enex,eney))
-
-    #     pygame.display.update()
-    #     fps.tick(60)
 
 
 if __name__ =='__main__':
     acceptC()
     GameMain()
+
